f6808e1eff8ce1ecc136ab8aba04f064503deeb7
NurastutiWijareni <nurastutiwijareni15@gmail.com>
2023-02-22 14:41:14 +0700

    add profile screen UI

--- lib/widgets/profile/profile_screen.dart
+++ lib/widgets/profile/profile_screen.dart
@@ -1,10 +1,160 @@
 import 'package:flutter/material.dart';
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({super.key});
 
   @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(10),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const CircleAvatar(
+              backgroundColor: Colors.grey,
+              radius: 50.0,
+              child: CircleAvatar(
+                backgroundColor: Colors.grey,
+                backgroundImage: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png'),
+                radius: 47.5,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                        labelText: 'Nama Lengkap',
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                        labelText: 'Tanggal Lahir',
+                        hintText: 'contoh: 2001-09-18',
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                        labelText: 'Email',
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+                        labelText: 'Password',
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width / 3,
+                          height: 50,
+                          decoration: const BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(1, 1),
+                                blurRadius: 1,
+                                color: Color(0xFF62BBE2),
+                              ),
+                            ],
+                          ),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF62BBE2),
+                              padding: const EdgeInsets.all(0),
+                            ),
+                            onPressed: () {},
+                            child: Row(
+                              children: const [
+                                SizedBox(width: 10),
+                                Icon(Icons.login_outlined),
+                                SizedBox(width: 15),
+                                Text('Masuk'),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width / 3,
+                          height: 50,
+                          decoration: const BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                offset: Offset(1, 1),
+                                blurRadius: 1,
+                                color: Color(0xFF62BBE2),
+                              ),
+                            ],
+                          ),
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF62BBE2),
+                              padding: const EdgeInsets.all(0),
+                            ),
+                            onPressed: () {},
+                            child: Row(
+                              children: const [
+                                SizedBox(width: 10),
+                                Icon(Icons.save_outlined),
+                                SizedBox(width: 15),
+                                Text('Simpan'),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
